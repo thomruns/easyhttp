91a6e43fa08dc9e984823c473c8e6c0bcde14495
Thom David <thomdavid74@gmail.com>
2018-03-29 12:21:07 -0700

    POST request added

--- easyhttp2.js
+++ easyhttp2.js
@@ -19,4 +19,20 @@ class EasyHTTP {
       .catch(err => reject(err));
     }); 
   }
+
+  // Make HTTP POST request
+  post(url, data) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      })
+      .then(res => res.json())
+      .then(data => resolve(data))
+      .catch(err => reject(err));
+    }); 
+  }
 }
